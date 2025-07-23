8caf03ce1fa17abce5a905082cffe637cc1fd4b2
Onesmas Kungu <42471079+onesmaskungu@users.noreply.github.com>
2025-07-23 19:57:34 +0300

    initial commit
    initial commit

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'stock-app-v1';
+const assets = [
+  './',
+  './index.html',
+  './minimal.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
